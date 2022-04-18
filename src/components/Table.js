538e09aa619185fb43993f6c810b08dca9d85dd6
ítalo Lima <contatoitallo@hotmail.com>
2022-04-18 20:39:21 -0300

    Cria input de texto e filtra a tabela através deste-Req 02

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,12 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 // import PropTypes from 'prop-types';
 import StarWarsContext from '../context/StarWarsContext';
 
 function Table() {
   const { data, fetchPlanets } = useContext(StarWarsContext);
   // eslint-disable-next-line react-hooks/exhaustive-deps
+  const [filterByName, setFilterByName] = useState('');
+  //   const [getingFilterByName, setGetingFilterByName] = useState({});
   useEffect(() => {
     async function getPlanets() {
       await fetchPlanets();
@@ -13,46 +15,88 @@ function Table() {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   //   console.log(data);
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>Url</th>
-        </tr>
-      </thead>
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const filterNamePlanet = (e) => {
+    // e.preventDefault();
+    setFilterByName(e.target.value);
+    // const filterPlanet = data.planets
+    //   .filter((planet) => (planet.name !== filterByName));
+    // setGetingFilterByName(filterPlanet);
+  };
 
-      {(data.planets.length > 0) && data.planets.map((planet, i) => (
-        <tbody key={ i }>
+  //   eslint-disable-next-line react-hooks/exhaustive-deps
+  //   useEffect(() => filterNamePlanet(), [filterByName]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        name="name"
+        data-testid="name-filter"
+        value={ filterByName }
+        placeholder="Escreva o nome do planeta..."
+        onChange={ filterNamePlanet }
+      />
+      <table>
+        <thead>
           <tr>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>{planet.films}</td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>Url</th>
           </tr>
-        </tbody>
-      ))}
-    </table>
+        </thead>
+
+        {(data.planets.length > 0) && (filterByName.length > 0) ? (data.planets
+          .filter((planet) => (planet.name.includes(filterByName))).map((planet, i) => (
+            <tbody key={ i }>
+              <tr>
+                <td>{planet.name}</td>
+                <td>{planet.rotation_period}</td>
+                <td>{planet.orbital_period}</td>
+                <td>{planet.diameter}</td>
+                <td>{planet.climate}</td>
+                <td>{planet.gravity}</td>
+                <td>{planet.terrain}</td>
+                <td>{planet.surface_water}</td>
+                <td>{planet.population}</td>
+                <td>{planet.films}</td>
+                <td>{planet.created}</td>
+                <td>{planet.edited}</td>
+                <td>{planet.url}</td>
+              </tr>
+            </tbody>
+          ))) : (data.planets
+          .map((planet) => (
+            <tbody key={ planet.url }>
+              <tr>
+                <td>{planet.name}</td>
+                <td>{planet.rotation_period}</td>
+                <td>{planet.orbital_period}</td>
+                <td>{planet.diameter}</td>
+                <td>{planet.climate}</td>
+                <td>{planet.gravity}</td>
+                <td>{planet.terrain}</td>
+                <td>{planet.surface_water}</td>
+                <td>{planet.population}</td>
+                <td>{planet.films}</td>
+                <td>{planet.created}</td>
+                <td>{planet.edited}</td>
+                <td>{planet.url}</td>
+              </tr>
+            </tbody>
+          )))}
+      </table>
+    </div>
   );
 }
 
@@ -62,3 +106,25 @@ function Table() {
 // };
 
 export default Table;
+
+// (getingFilterByName.filterPlanet.length > 0)
+//           ? (getingFilterByName.filterPlanet.map((planet, i) => (
+//             <tbody key={ i }>
+//               <tr>
+//                 <td>{planet.name}</td>
+//                 <td>{planet.rotation_period}</td>
+//                 <td>{planet.orbital_period}</td>
+//                 <td>{planet.diameter}</td>
+//                 <td>{planet.climate}</td>
+//                 <td>{planet.gravity}</td>
+//                 <td>{planet.terrain}</td>
+//                 <td>{planet.surface_water}</td>
+//                 <td>{planet.population}</td>
+//                 <td>{planet.films}</td>
+//                 <td>{planet.created}</td>
+//                 <td>{planet.edited}</td>
+//                 <td>{planet.url}</td>
+//               </tr>
+//             </tbody>
+//           )))
+//           :
